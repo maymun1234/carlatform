604e14e89856e23d6cddaf9105f3b77a6926b698
maymun1234 <143941941+maymun1234@users.noreply.github.com>
2024-03-09 23:43:09 +0300

    Add files via upload

--- ScoreManager.cs
+++ ScoreManager.cs
@@ -5,33 +5,58 @@ public class ScoreManager : MonoBehaviour
 {
     public string targetTag = "YourTargetTag"; // Hedef objelerin etiketi
     public int scorePerHit = 1; // Her vuruşta eklenecek skor miktarı
+    public int coinPerHit = 10; // Her vuruşta eklenecek skor miktarı
+
     public TMP_Text scoreText; // Skoru gösterecek TMPro nesnesi
     public TMP_Text highScoreText; // Yüksek skoru gösterecek TMPro nesnesi
+    public TMP_Text coinText; // Para miktarını gösterecek TMPro nesnesi
 
     private int score = 0; // Oyuncunun skoru
-    public int highScore = 0; // Yüksek skor
+    private int highScore = 0; // Yüksek skor
+    private int coin = 0; // Oyuncunun parası
+    
 
     private const string highScoreKey = "HighScore"; // PlayerPrefs anahtarı
+    private const string coinKey = "Coin";
 
     void Start()
     {
-        highScore = PlayerPrefs.GetInt(highScoreKey, 0); // Yüksek skoru al, eğer kayıtlı değilse 0 olarak ayarla
-    UpdateScoreText();
-    UpdateHighScoreText(); // Yüksek skoru gösteren metni güncelle
+        // Skoru sıfırla
+        score = 0;
+        UpdateScoreText();
+        scoreText.text = score.ToString();
 
+        // Highscore'u ve coini veritabanından çek
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+        coin = PlayerPrefs.GetInt(coinKey, 0);
+
+        // Metin alanlarını güncelle
+        UpdateHighScoreText();
+        UpdateCoinText();
+    }
+
+    void Update()
+    {
+        // Her güncellemede highscore'u kontrol et
+        
     }
 
     // Hedef objeye çarpıldığında çağrılacak olan metot
     public void HitTarget()
     {
         score += scorePerHit; // Skora puan ekle
-        if (score > highScore) // Eğer mevcut skor yüksek skoru aşıyorsa
+        coin += coinPerHit; // Para miktarını güncelle
+        UpdateCoinText();
+        if (score > highScore)
         {
-            highScore = score; // Yüksek skoru güncelle
-            PlayerPrefs.SetInt(highScoreKey, highScore); // Yüksek skoru kaydet
-            UpdateHighScoreText(); // Yüksek skor metnini güncelle
+            highScore = score;
+            PlayerPrefs.SetInt(highScoreKey, highScore);
+            UpdateHighScoreText();
         }
-        UpdateScoreText(); // Skor metnini güncelle
+
+        // Metin alanlarını güncelle
+        UpdateScoreText();
+        
     }
 
     // Skor metnini güncelleyen metot
@@ -39,7 +64,7 @@ public class ScoreManager : MonoBehaviour
     {
         if (scoreText != null)
         {
-            scoreText.text = "" + score.ToString(); // Skor metnini güncelle
+            scoreText.text = score.ToString(); // Skor metnini güncelle
         }
     }
 
@@ -48,7 +73,16 @@ public class ScoreManager : MonoBehaviour
     {
         if (highScoreText != null)
         {
-            highScoreText.text = "" + highScore.ToString(); // Yüksek skor metnini güncelle
+            highScoreText.text = highScore.ToString(); // Yüksek skor metnini güncelle
+        }
+    }
+
+    // Para metnini güncelleyen metot
+    void UpdateCoinText()
+    {
+        if (coinText != null)
+        {
+            coinText.text = coin.ToString(); // Para miktarı metnini güncelle
         }
     }
 
